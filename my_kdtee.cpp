24217bbf9a7ac60bb62d8258abb2f2399ea27204
leoshen <chengzhi.shen@outlook.com>
2021-12-04 11:23:26 +0800

    using std::vector

--- my_kdtee.cpp
+++ my_kdtee.cpp
@@ -1,111 +1,125 @@
 #include<bits/stdc++.h>
 #include<iostream>
+#include<string>
 #include<vector>
 
 #define K 2
 
 
-// Each node in a KD-tree saves: current node data, pointer to the left node, poiter to the right node
+// reload << to better output vectors
+std::ostream& operator<<(std::ostream& os, std::vector<double> vec)
+{
+    for(const auto& elem : vec)
+    {
+        os << elem << " " ;
+    }
+    os << '\n';
+    return os;
+}
+
+
+
+// Each node in a KD-tree has: current node data, pointer to the left node, poiter to the right node
 struct Node
 {
-    // std::vector<double> point(k);
-	double point[K]; // To store k dimensional point
+    std::vector<double> point;
 	Node *left;
     Node *right;
 };
 
-// A method to create a node of K D tree
-struct Node* newNode(double arr[])
+
+// create a new node 
+Node* newNode(std::vector<double> in_arr)
 {
 	struct Node* temp = new Node;
-
-	for (int i=0; i<K; i++)
-	temp->point[i] = arr[i];
-
+    temp->point = in_arr;
 	temp->left = temp->right = NULL;
+    // std::cout << "in newNode: " << temp->point << std::endl;
 	return temp;
 }
 
 // Inserts a new node and returns root of modified tree
 // The parameter depth is used to decide axis of comparison
-Node *insertRec(Node *root, double point[], unsigned int depth)
+Node* insert(Node *root, std::vector<double> point, unsigned int depth=0)
 {
 	// Tree is empty?
 	if (root == NULL)
-	return newNode(point);
-
+    {
+        // std::cout << "Tree is empty, start constructing..." << std::endl;
+    	return newNode(point);
+    }
 	// Calculate current dimension (cd) of comparison
 	unsigned cd = depth % K;
 
 	// Compare the new point with root on current dimension 'cd'
 	// and decide the left or right subtree
 	if (point[cd] < (root->point[cd]))
-		root->left = insertRec(root->left, point, depth + 1);
+    {
+        root->left = insert(root->left, point, depth + 1);
+    }
 	else
-		root->right = insertRec(root->right, point, depth + 1);
-
+    {
+        root->right = insert(root->right, point, depth + 1);
+    }
 	return root;
 }
 
-// Function to insert a new point with given point in
-// KD Tree and return new root. It mainly uses above recursive
-// function "insertRec()"
-Node* insert(Node *root, double point[])
+class Utilities
 {
-	return insertRec(root, point, 0);
-}
+    // A class of utility functions 
+public:
+    bool arePointsSame(std::vector<double> point1, std::vector<double> point2)
+    // determine if two Points are same
+    {
+        for (int i = 0; i < K; ++i)
+            if (point1[i] != point2[i])
+                return false;
 
-// A utility method to determine if two Points are same
-// in K Dimensional space
-bool arePointsSame(double point1[], double point2[])
-{
-	// Compare individual pointinate values
-	for (int i = 0; i < K; ++i)
-		if (point1[i] != point2[i])
-			return false;
+        return true;
+    }
 
-	return true;
-}
+    bool search(Node* root, std::vector<double> point, unsigned depth=0)
+    {
+    // search if the point exists in the KD-tree
+        if (root == NULL)
+            return false;
+        if (arePointsSame(root->point, point))
+            return true;
 
-// Searches a Point represented by "point[]" in the K D tree.
-// The parameter depth is used to determine current axis
-bool search(Node* root, double point[], unsigned depth=0)
-{
-// Base cases
-	if (root == NULL)
-		return false;
-	if (arePointsSame(root->point, point))
-		return true;
+        // Current dimension is computed using current depth and total
+        // dimensions (k)
+        unsigned cd = depth % K;
 
-	// Current dimension is computed using current depth and total
-	// dimensions (k)
-	unsigned cd = depth % K;
+        // Compare point with root with respect to cd (Current dimension)
+        if (point[cd] < root->point[cd])
+            return search(root->left, point, depth + 1);
 
-	// Compare point with root with respect to cd (Current dimension)
-	if (point[cd] < root->point[cd])
-		return search(root->left, point, depth + 1);
+        return search(root->right, point, depth + 1);
+    }
 
-	return search(root->right, point, depth + 1);
+};
 
-}
 
-// Driver program to test above functions
 int main()
 {
 	struct Node *root = NULL;
-	double points[][K] = {{3, 6}, {17, 15}, {13, 15}, {6, 12},
+    Utilities utils;
+	std::vector<std::vector<double>> points_Arr = {{3, 6}, {17, 15}, {13, 15}, {6, 12},
 					{9, 1}, {2, 7}, {10, 19}};
 
-	int n = sizeof(points)/sizeof(points[0]);
+    
+    auto n = points_Arr.size();
 
 	for (int i=0; i<n; i++)
-	root = insert(root, points[i]);
+    {
+        root = insert(root, points_Arr[i]);
+    }
 
-	double point1[] = {10, 19};
-	(search(root, point1))? std::cout << "Found\n": std::cout << "Not Found\n";
+	std::vector<double> point1 = {10, 19};
+	(utils.search(root, point1))? std::cout << "Found\n": std::cout << "Not Found\n";
 
-	double point2[] = {12, 19};
-	(search(root, point2))? std::cout << "Found\n": std::cout << "Not Found\n";
+	std::vector<double> point2 = {12, 19};
+	(utils.search(root, point2))? std::cout << "Found\n": std::cout << "Not Found\n";
 
 	return 0;
 }

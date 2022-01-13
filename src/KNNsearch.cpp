cab654e7f325dcc151e56d39cb99ea5d1f151cf8
Amr Abuzer <asnralpha@gmail.com>
2022-01-13 22:24:22 +0100

    NN search v1

---
+++ src/KNNsearch.cpp
@@ -0,0 +1,67 @@
+// Basic Plan:
+// 1- We want to make methods first:
+//      i- Define a rectangle class - done
+//      ii- Define a distance function from point to point and point to rectangle - done
+//      iii- Find initial d(Q,C), here Q is the query point, C is the node which our Q would've been under if we traversed the tree for and d is our 
+//          squared distance function - done
+//      iv- Method to define the bounding box: simply >< immediate parent node[cd] - done
+//      v- We recursively now check the parent nodes:
+//                  if bounding box of node < d(Q,C) we recursively check the left and right children of our node
+//                  else we prune the node from our search
+//      
+// 2- Turn our method into a class
+// 3- Create a test case (we can try to use the test case in the slides)
+// 4- Not done for k>2 points
+
+#include<iostream>
+#include "my_kd_tree.h"
+
+std::vector<double> Q;
+struct Rect
+{
+    int Xmin, Xmax;
+    int Ymin, Ymax;  
+    trimRight(int cd, std::vector<double> X){
+        if(cd%2==0){ Xmin=X[0]; }
+        else{Ymin=X[1];}
+    }
+    trimLeft(int cd, std::vector<double> X){
+        if(cd%2==0){ Xmax=X[0]; }
+        else{Ymax=X[1];}
+    }
+};
+
+int distance(auto Q,Rect BB, int k=2){
+    dx=std::max(BB.Xmin-Q[0],0,BB.Xmax-Q[0]);   
+    dy=std::min(BB.Ymin-Q[1],0,BB.Ymax-Q[1]);
+    return dx*dx+dy*dy;
+}
+
+int distance(auto Q,auto point, int k=2){
+    return (Q[0]-point[0])^2 +(Q[1]-point[1])^2;
+}
+Node* parentNode= insert(Q,root,0);
+
+best_dist=distance(Q, parentNode.point);
+
+
+Node* getNN(auto Q, Node* T, int cd=0, Rect BB)
+    {
+// if this bounding box is too far, do nothing
+    if (T == NULL || distance(Q, BB) > best_dist) return;
+// if this point is better than the best:
+    dist = distance(Q, T.point)
+    if (dist < best_dist)
+        {best = return T;
+        best_dist = dist;
+        }
+// visit subtrees in most promising order:
+    if (Q[cd] < T.point[cd]){
+        NN(Q, T.right, (cd+1)%k, BB.trimRight(cd, T.point));
+        NN(Q, T.left, (cd+1)%k, BB.trimLeft(cd, T.point));
+        }
+    else{
+        NN(Q, T.right, (cd+1)%k, BB.trimRight(cd, T.point));
+        NN(Q, T.left, (cd+1)%k, BB.trimLeft(cd, T.point));
+        }
+    }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
-#include<fstream>
+#include <fstream>
 #include "my_kd_tree.h"
 
 
@@ -13,28 +13,14 @@ int main()
 {
     Node *root = NULL;
 
-    // generate toy data
     auto value_vectors =generate_numbers(100);
 
-    // // write generated data to csv
     write_to_csv(value_vectors, filepath);
 
-    // read data as vector of vectors from csv file
-    // auto value_vectors = read_from_csv(filepath);
-
-    // print vectors
-    // for (auto &elem : value_vectors)
-    // {
-    //     print_vector(elem);
-    // }
-
-    // construct the tree
     for (auto &elem : value_vectors)
     {
         root = insert(elem, root);
     }
     print_kd_tree("",root,false);
 
-    // Node *new_root = delete_node(value_vectors, value_vectors[0]);
-    // print_kd_tree("",new_root,false);
 }
